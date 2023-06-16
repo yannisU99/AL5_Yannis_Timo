052d6f6d3ed7ae7b1a1ff02b1818d8d31077b10d
yannisU99 <132067185+yannisU99@users.noreply.github.com>
2023-06-16 16:48:03 +0200

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,93 @@
+const dino = document.getElementById("game-dino");
+const rock = document.getElementById("game-rock");
+const score = document.getElementById("game-score");
+const gameBox = document.getElementById("game");
+const background = document.getElementById("game-background");
+const gameOver = document.getElementById("game-end");
+const winnerText = document.getElementById("game-winner");
+const startScreen = document.getElementById("game-start");
+
+let gameLoopInterval = 0;
+const POINTS_TO_WIN = 100;
+
+const startGame = () => {
+  gameOver.classList.add("hidden");
+  background.classList.add("bg-animation");
+  rock.classList.add("rock-animation");
+  startScreen.classList.add("hidden");
+  resetScore();
+  startGameLoop();
+};
+
+const resetScore = () => {
+  score.innerText = 0;
+};
+
+const jump = () => {
+  dino.classList.add("jump-animation");
+  setTimeout(() => {
+    dino.classList.remove("jump-animation");
+  }, 500);
+};
+
+const dieAnimation = () => {
+  dino.style.backgroundImage = "url('/images/game/dino2_dead.png')";
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      dino.style.backgroundImage = "url('/images/game/dino2.png')";
+      resolve();
+    }, 500)
+  );
+};
+
+
+
+gameBox.addEventListener("click", (event) => {
+  if (!gameLoopInterval) {
+    startGame();
+  } else {
+    if (!dino.classList.contains("jump-animation")) {
+      jump();
+    }
+  }
+});
+
+const stopGame = async () => {
+  await dieAnimation();
+  background.classList.remove("bg-animation");
+  rock.classList.remove("rock-animation");
+  startScreen.classList.remove("hidden");
+  gameLoopInterval = clearInterval(gameLoopInterval);
+  gameOver.classList.remove("hidden");
+  if (Number(score.innerText) + 1 >= POINTS_TO_WIN) {
+    winnerText.classList.remove("hidden");
+  }
+
+  // Code zum Anzeigen der right-box
+  const rightBox = document.getElementById("right-box");
+  rightBox.classList.remove("hidden");
+};
+
+
+const startGameLoop = () => {
+  gameLoopInterval = window.setInterval(() => {
+    const dinoTop = parseInt(
+      window.getComputedStyle(dino).getPropertyValue("top")
+    );
+    const rockLeft = parseInt(
+      window.getComputedStyle(rock).getPropertyValue("left")
+    );
+
+    score.innerText = Number(score.innerText) + 1;
+
+    if (rockLeft < 0) {
+      rock.classList.add("hidden");
+    } else {
+      rock.classList.remove("hidden");
+    }
+
+    if (rockLeft < 50 && rockLeft > 0 && dinoTop > 150) {
+      stopGame();
+    }
+  }, 50);
+};
